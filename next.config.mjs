2e3f816897ab9c9ddfb1d64f7231a39460ae108b
Addan Sohail <addansohail8@gmail.com>
2025-03-17 00:40:09 -0500

    fix for live site no response with buttons

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Add this line for static site generation
+  basePath: '/cpu-scheduling-simulator', // Add this line for subpath hosting
 };
 
 export default nextConfig;
