8586ed736fb0ab9e0515b118c24aa8dd4d0c7676
Addan Sohail <addansohail8@gmail.com>
2025-03-16 23:02:32 -0500

    Updated the _document.js file

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default function Document() 
+{
   return (
     <Html lang="en">
       <Head />
@@ -10,4 +11,5 @@ export default function Document() {
       </body>
     </Html>
   );
+  
 }
